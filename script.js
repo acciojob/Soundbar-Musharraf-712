46999a6793724954fc0dd9d809a54c873a8e3d81
Musharraf-712 <musharrafsiddiqui712@gmail.com>
2025-10-25 11:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+// List of sounds
+const sounds = [
+    "sound1.mp3",
+    "sound2.mp3",
+    "sound3.mp3",
+    "sound4.mp3"
+];
+
+document.querySelectorAll(".btn").forEach((button, i) => {
+    const audio = new Audio(`./sounds/${sounds[i]}`);
+
+    button.addEventListener("click", () => {
+        stopAllSounds();
+        audio.play();
+    });
+});
+
+document.querySelector(".stop").addEventListener("click", stopAllSounds);
+
+// Stop all sounds
+function stopAllSounds() {
+    const audios = document.querySelectorAll("audio");
+    audios.forEach(sound => {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
